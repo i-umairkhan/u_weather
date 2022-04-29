dbb4b96bfcce7ddb49c4246a80aa7a5e0e918f71
umair2607 <umair.k2607@gmail.com>
2022-04-29 21:18:20 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Uweather());
+}
+
+class Uweather extends StatelessWidget {
+  const Uweather({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
